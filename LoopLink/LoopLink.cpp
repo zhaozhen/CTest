a0bba842536832ff56fa8ea2936baaaee20a95f8
zhen <286339409@qq.com>
2017-04-19 22:42:23 +0800

    的好难用呀 我就是i

--- LoopLink/LoopLink.cpp
+++ LoopLink/LoopLink.cpp
@@ -1,7 +1,7 @@
-#include <studio.h>
+#include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <stdloob.h>
+#include <stdbool.h>
 
 struct node
 {
@@ -10,7 +10,7 @@ struct node
 	struct node *next;
 };
 
-struct node *head=NULL
+struct node *head=NULL;
 struct node *current=NULL;
 
 //panduan is  empty
@@ -51,8 +51,43 @@ struct node* deleteFirst(){
 		head=NULL;
 		return tempLink;
 	}
+	head=head->next;
+	return tempLink;
 }
 
-void pritLis(){
-	
+void pritList(){
+	struct node* ptr=head;
+	printf("\n[");
+
+	if(head!=NULL){
+		while(ptr->next!=ptr){
+			printf("(%d,%d)",ptr->key,ptr->data);
+			ptr=ptr->next;
+		}
+	}
+	printf("]\n");
+
+}
+int main(int argc, char const *argv[])
+{
+	insertFist(1,10);
+	insertFist(2,20);
+	insertFist(3,30);
+	insertFist(4,1);
+	insertFist(5,4);
+	insertFist(6,56);
+
+	printf("print Origin List\n");
+	pritList();
+
+	while(!isEmpty()){
+		struct node* temp=deleteFirst();
+		printf("delete the value");
+		printf("(%d,%d)\n",temp->key,temp->data);
+	}
+
+	printf("delete after");
+	pritList();
+
+	return 0;
 }
