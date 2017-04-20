fcadfe7f874a2e63bd0c9f1c683841aaeae98cba
zhen <286339409@qq.com>
2017-04-20 22:03:40 +0800

    添加昨天没有写完的双向链表添加，删除，

--- TwoLink/twoLink.cpp
+++ TwoLink/twoLink.cpp
@@ -1,1 +1,47 @@
 今天的操作全部错误的删除了，明天再继续搞，也没有带鼠标
+//today is back
+void insertFist(int key,int data){
+	struct node* link=(struct node*)malloc(struct node);
+	link->key=key;
+	link->data=data;
+
+	if(isEmpty()){
+		last=link;
+	}else{
+		head->prev=link;
+	}
+	link->next=head;
+	head=link;
+}
+//delete operation
+struct node* deletFist(){
+	struct node* tempLink=head;
+
+	if(head->next==NULL){
+		last=NULL;
+	}else{
+		head->next->prev=NULL;
+	}
+
+	head=head->next;
+	return tempLink;
+}
+
+void insertLast(int key,int data){
+	struct node* link=(struct node*)malloc(struct node);
+
+	link->key=key;
+	link->data=data;
+
+	if(isEmpty()){
+		last=link;
+	}else{
+		last->next=link;
+		link->prev=last;
+	}
+	last=link;
+
+}
+
+
+
